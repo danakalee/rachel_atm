6ad972903fcb1c7e89ce1f57d099941e96f0c539
Rachel <hello.rachelwong@gmail.com>
2019-03-31 13:26:41 +1000

    changed if statement

--- atm_model.rb
+++ atm_model.rb
@@ -5,9 +5,12 @@ class ATMModel
     end
 
     def withdraw_balance(balance, withdraw_amount)
-        if withdraw_amount < balance
-            balance = balance - withdraw_amount
-            return balance
+        if balance >= withdraw_amount # if balance is greater than or equal to the withdrawal amount
+            balance = balance - withdraw_amount # proceed with withdrawal request
+            return balance # return udpated balance to controller
+        else
+            return balance # return the balance unchanged
+        # else, strictly speaking view should output an overdrawn error screen  
         end
     end
 end
